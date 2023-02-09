1e0740728efb108c45066174a614dcd215d58d50
letdtcode <boynguyenducthanh@gmail.com>
2023-02-10 01:41:15 +0700

    update UI

---
+++ app/src/main/java/com/community/realtime/activities/UsersActivity.java
@@ -0,0 +1,16 @@
+package com.community.realtime.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.community.realtime.R;
+
+public class UsersActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_users);
+    }
+}
